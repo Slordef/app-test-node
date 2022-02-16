c0b59b917abf4a02cc756fea7e3aa08879791aac
Maximilien Valenzano <slordef.vikamet@gmail.com>
2022-02-16 22:45:22 +0100

    Refacto of index.js for host and port

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 
+const port = 3000
+const host = '0.0.0.0'
+
 app.get('*', (req, res) => {
     res.json({
         name: 'Test app'
     })
 })
 
-server.listen(3380)
+server.listen(port, host, () => {
+    console.log(`Server started on http://${host}:${port}`)
+})
